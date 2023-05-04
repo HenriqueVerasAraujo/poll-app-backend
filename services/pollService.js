97dabc0f9c2f3967fba939136c591eb1d728ba3e
Henrique_Veras <theveras1@gmail.com>
2023-05-04 11:08:38 -0300

    updated fetchOnePoll

--- services/pollService.js
+++ services/pollService.js
@@ -1,4 +1,4 @@
-const { Poll, Item} = require('../models');
+const { Poll, Item, User } = require('../models');
 
 require('dotenv').config();
 
@@ -9,19 +9,30 @@ const createPoll = async(body, userId) => {
     await Promise.all(items
         .map(async (singleItem) => Item
         .create({ pollId: newPoll.id, itemTitle: singleItem.itemTitle, votes: 0 })));
+
     return newPoll;
 };
 
 const fetchAllPolls = async(userId) => {
     const allPolls = await Poll.findAll({ where: { userId } });
+
     return allPolls;
 };
 
 const fetchOnePoll = async(pollId) => {
-    const singlePoll = await Poll.findOne({ where: { id: pollId } });
+    const singlePoll = await Poll.findOne({
+        where: { id: pollId },
+        attributes: { exclude: ["userId", "updatedAt"] },
+        include: [
+            {model: Item, as: "items", attributes: { exclude: ["pollId"] }},
+            {model: User, as: "user", attributes: { exclude: ["id", "email", "password"] }},
+        ]
+    });
+
     if (!singlePoll) {
-        return {errMessage: 'Poll not found'}
-    }
+        return {errMessage: 'Poll not found'};
+    };
+
     return singlePoll;
 };
 
