164796edff225e9b665f12066cb45bc915054760
Henrique_Veras <theveras1@gmail.com>
2023-05-08 18:35:08 -0300

    Success message done

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ const loginUser = async (req, res) => {
         const tryLogin = await userService.loginUser(body);
         if (tryLogin.errMessage) {
             return res.json({ errMessage: tryLogin.errMessage });
-        }
+        };
         return res.json({token: tryLogin.token});
     } catch(err) {
         console.log(err);
@@ -22,8 +22,8 @@ const createUser = async (req, res) => {
         const createNewUser = await userService.createUser(body);
         if (createNewUser.errMessage) {
             return res.json({ errMessage: createNewUser.errMessage });
-        }
-        return res.json({message: 'User Created'});
+        };
+        return res.json({message: 'Account created!'});
     } catch(err) {
         console.log(err);
         return res.status(500).json({error: err});
