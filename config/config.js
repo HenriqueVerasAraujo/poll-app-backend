6b1ef519dd1b53d711d6c99cc6fb5f47f487ff05
Henrique_Veras <theveras1@gmail.com>
2023-06-01 19:30:05 -0300

    Test

--- config/config.js
+++ config/config.js
@@ -6,21 +6,23 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.DB,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
   },
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.DB,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.DB,
     host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
   },
 };
